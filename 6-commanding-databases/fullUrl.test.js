8492eb91965e1f529b02f240efe528d97718f885
Thanik Bhongbhibhat <thanikb@pinterest.com>
2018-08-01 09:41:16 -0700

    Chapter 6: Commanding Databases (4) - add test coverage "fullUrl"

--- 6-commanding-databases/fullUrl.test.js
+++ 6-commanding-databases/fullUrl.test.js
@@ -6,7 +6,30 @@ const program = require('commander');
 const fullUrl = require('./fullUrl');
 
 describe('fullUrl', () => {
-  it('returns the correct result', () => {
+  beforeEach(() => {
+    Object.assign(program, { host: null, port: null, index: null, type: null });
+  });
+
+  it('returns the correct path when no optional options are provided', () => {
+    Object.assign(program, { host: 'example.com' });
+    expect(fullUrl('harry-potter')).toBe('http://example.com/harry-potter');
+  });
+
+  it('returns the correct path with index but no type', () => {
+    Object.assign(program, { host: 'example.com', index: 'i318' });
+    expect(fullUrl('harry-potter')).toBe(
+      'http://example.com/i318/harry-potter'
+    );
+  });
+
+  it('returns the correct path with type but no index', () => {
+    Object.assign(program, { host: 'example.com', type: 'movies' });
+    expect(fullUrl('harry-potter')).toBe(
+      'http://example.com/movies/harry-potter'
+    );
+  });
+
+  it('returns the correct path when all options are provided', () => {
     Object.assign(program, {
       host: 'google.com',
       port: 8080,
