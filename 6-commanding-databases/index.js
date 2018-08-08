79aecc5b2b6f9baad50f58653033795d70ef1d64
Thanik Bhongbhibhat <email@thanik.me>
2018-08-08 16:36:52 -0700

    Chapter 6: Commanding Databases(6) - add commands get, create-index, list-indices

--- 6-commanding-databases/index.js
+++ 6-commanding-databases/index.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // const fs = require('fs');
-// const request = require('request');
+const request = require('request');
 const program = require('commander');
 const pkg = require('../package.json');
 const fullUrl = require('./fullUrl');
@@ -23,6 +23,57 @@ program
   )
   .action((path = '/') => {
     console.log(fullUrl(path));
+  });
+
+function handleResponse(err, res, body) {
+  if (program.json) {
+    console.log(JSON.stringify(err || body));
+  } else {
+    if (err) {
+      throw err;
+    }
+    console.log(body);
+  }
+}
+
+program
+  .command('get [path]')
+  .description('perform an HTTP GET request for path (default path is "/")')
+  .action((path = '/') => {
+    const options = {
+      url: fullUrl(path),
+      json: program.json,
+    };
+    request.get(options, handleResponse);
+  });
+
+program
+  .command('create-index')
+  .description('create an index')
+  .action(() => {
+    if (!program.index) {
+      const msg = 'No index specified! Use --index <name>';
+      if (!program.json) {
+        throw Error(msg);
+      }
+      console.log(JSON.stringify({ error: msg }));
+      return;
+    }
+
+    request.put(fullUrl(), handleResponse);
+  });
+
+program
+  .command('list-indices')
+  .alias('li')
+  .description('get a list of indices in this cluster')
+  .action(() => {
+    const path = program.json ? '_all' : '_cat/indices?v';
+    const options = {
+      url: fullUrl(path),
+      json: program.json,
+    };
+    request.get(options, handleResponse);
   });
 
 program.parse(process.argv);
