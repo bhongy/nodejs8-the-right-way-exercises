39b0a1423073228029781082311eee50e43a9567
Thanik Bhongbhibhat <email@thanik.me>
2018-08-22 21:11:55 -0700

    Chapter 6: Commanding Databases (10) - tests (not working)

--- 6-commanding-databases/esclu.test.js
+++ 6-commanding-databases/esclu.test.js
@@ -1,6 +1,9 @@
 'use strict';
 
+jest.mock('request');
+
 const path = require('path');
+const request = require('request');
 const child_process = require('child_process');
 const { promisify } = require('util');
 const execFile = promisify(child_process.execFile);
@@ -21,6 +24,38 @@ describe('esclu CLI', () => {
     const args = [command, path];
     const { stdout } = await execFile(esclu, args);
     expect(stdout).toContain('http://localhost:9200/chocolate-cake');
+  });
+
+  // test currently failing because jest doesn't mock "request" module correctly
+  // (it hits the real endpoint) maybe because it gets invoked
+  // via child_process.exec ?
+  it('supports get command', async () => {
+    expect.assertions(1);
+
+    request.get.mockImplementation((options, cb) => {
+      cb(null, {}, { foo: 'bar' });
+    });
+      
+    const command = 'get';
+    const path = 'fruit-smoothie';
+    const args = [command, path];
+    const { stdout } = await execFile(esclu, args);
+    expect(stdout).toContain('{"foo": "bar"}');
+  });
+
+  // but this passes
+  it('... TEMPORARY ... check "request" is mocked', done => {
+    request.get.mockImplementation((options, cb) => {
+      cb(null, {}, { foo: 'bar' });
+    });
+    const options = {
+      url: 'http://localhost:9200/fruit-smoothie',
+      json: undefined,
+    };
+    request.get(options, (err, socket, data) => {
+      expect(data).toEqual({ foo: 'bar' });
+      done();
+    });
   });
 
   it('throws when passing unsupported option', async () => {
